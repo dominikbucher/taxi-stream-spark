86a8a15fb2438615b6f2a3e43f2b3a43cac41f1c
dominik.bucher <dominik.bucher@gmail.com>
2018-05-15 14:54:05 +0200

    Outlined test_streaming_architecture.py.

--- scripts/test_streaming_architecture.py
+++ scripts/test_streaming_architecture.py
@@ -13,13 +13,24 @@ master_ip = '123.123.123.123'
 
 def test_application(architecture, batch_time, taxi_throughput, client_throughput, num_workers):
     # Set the necessary parameters in the Go Streaming config.
+    config = {
+    '': 3000
+    }
+
+    json.from_dict(config)   
+    cmd_start_master = r'go.exe'
+    pid1 = subprocess.call(cmd_start_master, shell=True, cwd=directory) 
 
     # Set the number of workers (spark.executor.instances system property).
+    sys.set_envvar('spark.streaming.blockInterval')
 
     # Submit the job to the master (How do we collect data? Can the master dump everything after it ran for
     # a minute or so?).
+    cmd_start_master = r'spark-deploy'
+    pid2 = subprocess.call(cmd_start_master, shell=True, cwd=directory) 
 
     # Notify the script user of progress.
+    timeout(10000, kill pid1, pid2)
     pass
 
 
